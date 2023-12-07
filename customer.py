b159a1febd0c4af52f0d493d56509ca6d92b7343
Eric McKnight <emcknig8@students.kennesaw.edu>
2023-12-07 13:35:00 -0500

    Customer phone number randomizer fix & Class notes

--- customer.py
+++ customer.py
@@ -1,10 +1,15 @@
 import names
 import random
 
-## Customer Class ##
+'''
+Mailchimp Backend Service Scheduler Assignment - Customer Class
+---------------------------------------------------------------
+This is the prescribed customer class used to store information about each customer being checked in. Information includes a fully randomized name,
+phone number, and whether or not the customer is a VIP or not. Ticket ID is then only piece of data passed into the function for storage.
+'''
 class Customer:
     def __init__(self, ticketId = None):
         self.name = names.get_full_name()  # Random Name of customer 
-        self.phoneNum = ''.join(["{}".format(random.randint(0, 9)) for num in range(0, 9)])  # Random Phone Number of customer
+        self.phoneNum = ''.join(["{}".format(random.randint(0, 9)) for num in range(0, 10)])  # Random 10 digit Phone Number of customer
         self.isVIP = random.getrandbits(1) == 1  # Randomized Boolean Indicating if a customer is a VIP customer or not
         self.ticketId = ticketId # Ticket ID auto assigned at customer creation
